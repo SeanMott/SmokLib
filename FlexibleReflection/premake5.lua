04dff7de5a88b614daffb71c3b514bbf25050e46
SeanMott <rafal.mott1@gmail.com>
2020-08-22 14:03:52 -0500

    Added FlexiableReflection for reflections

---
+++ FlexibleReflection/premake5.lua
@@ -0,0 +1,30 @@
+project "FlexibleReflection"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"Primitives.cpp",
+		"Reflect.h"
+	}
+	
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+		staticruntime "On"
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
